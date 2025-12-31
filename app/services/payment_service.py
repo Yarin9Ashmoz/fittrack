d1cb70f63170a9af423545146b0be1669b9753be
Yarin9Ashmoz <154062144+Yarin9Ashmoz@users.noreply.github.com>
2025-12-31 11:08:44 +0200

    payment_service - done

---
+++ app/services/payment_service.py
@@ -0,0 +1,58 @@
+from app.db.database import engine
+from app.models.payment import payments
+from app.models.subscription import subscriptions
+from app import exceptions
+from sqlalchemy import select, update
+
+
+def create_payment(data: dict):
+    subscription_id = data["subscription_id"]
+
+    with engine.connect() as conn:
+
+        sub = conn.execute(
+            select(subscriptions).where(subscriptions.c.id == subscription_id)
+        ).fetchone()
+
+        if not sub:
+            raise exceptions.NotFoundError("Subscription not found")
+
+        result = conn.execute(payments.insert().values(**data))
+        new_id = result.lastrowid
+
+        return get_payment_by_id(new_id)
+
+
+def get_payment_by_id(payment_id: int):
+    with engine.connect() as conn:
+        row = conn.execute(
+            select(payments).where(payments.c.id == payment_id)
+        ).fetchone()
+
+        if not row:
+            raise exceptions.NotFoundError("Payment not found")
+
+        return row
+
+
+def get_payments_by_member(member_id: int):
+    with engine.connect() as conn:
+        query = (
+            select(payments)
+            .select_from(payments.join(subscriptions))
+            .where(subscriptions.c.user_id == member_id)
+        )
+        return conn.execute(query).fetchall()
+
+
+def cancel_payment(payment_id: int):
+    with engine.connect() as conn:
+        get_payment_by_id(payment_id)
+
+        conn.execute(
+            update(payments)
+            .where(payments.c.id == payment_id)
+            .values(status="canceled")
+        )
+
+        return get_payment_by_id(payment_id)
